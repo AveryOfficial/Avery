e5e5666597fd4d9a9b91af7173bb7be9f576f0f4
AveryOfficial <111689872+AveryOfficial@users.noreply.github.com>
2022-08-21 18:13:35 -0400

    Ported over some code from a previous experiment
    I added in some extremely basic cairo painting ability. It's extremely rudimentary, needs to be greatly enhanced. I also need to fix the commenting later.

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -2,6 +2,21 @@
 #define MAINWINDOW_H
 
 #include <gtk/gtk.h>
+
+//Clears the surface
+static void SurfaceClear(void);
+
+//Creates a brand new surface
+static gboolean ConfigureEventCB(GtkWidget *Widget, GdkEventConfigure *Event, gpointer UserData);
+
+static gboolean DrawCB(GtkWidget *Widget, cairo_t *CR, gpointer UserData);
+
+static gboolean ButtonPressEventCB(GtkWidget *Widget, GdkEventButton *Event, gpointer UserData);
+
+
+
+
+
 void InitUI(GtkApplication *App, gpointer UserData);
 
 #endif
--- src/gui/mainwindow.c
+++ src/gui/mainwindow.c
@@ -2,6 +2,87 @@
 #include <stdio.h>
 #include "../../include/mainwindow.h"
 
+//What you will be drawing on
+static cairo_t *Surface = NULL;
+
+
+//The Cairo stuff right now is from an online example, will have to clean it up later because it's extremely basic.
+
+//This clears the surface
+static void SurfaceClear(void) {
+    cairo_t *CR;
+
+    CR = cairo_create(Surface);
+
+    cairo_set_source_rgb(CR, 1, 1, 1);
+
+    cairo_paint(CR);
+
+    cairo_destroy(CR);
+}
+
+//Draws the surface
+static gboolean ConfigureEventCB(GtkWidget *Widget, GdkEventConfigure *Event, gpointer UserData) {
+    cairo_surface_t *OldSurface = Surface;
+    cairo_t *CR;
+    Surface = gdk_window_create_similar_surface(gtk_widget_get_window(Widget), CAIRO_CONTENT_COLOR, gdk_screen_width(), gdk_screen_height());
+
+    //Initialize to white
+    SurfaceClear();
+    if (OldSurface) {
+        CR = cairo_create(Surface);
+        cairo_set_source_surface(CR, OldSurface, 0, 0);
+        cairo_set_operator(CR, CAIRO_OPERATOR_SOURCE);
+
+        cairo_paint(CR);
+        cairo_destroy(CR);
+        cairo_surface_destroy(OldSurface);
+    }
+    return TRUE;
+}
+
+static gboolean DrawCB(GtkWidget *Widget, cairo_t *CR,gpointer UserData) {
+    cairo_set_source_surface(CR, Surface, 0, 0);
+    cairo_paint(CR);
+
+    return FALSE;
+}
+
+
+
+//Draw a rectangle on the surface on any given position
+static void DrawingBrush(GtkWidget *Widget,
+        gdouble x, gdouble y) {
+    cairo_t *CR;
+
+        CR = cairo_create(Surface);
+
+        cairo_rectangle(CR, x - 3, y - 3, 6, 6);
+        cairo_fill(CR);
+
+        cairo_destroy(CR);
+
+        gtk_widget_queue_draw_area(Widget, x - 3, y - 3, 6, 6);
+}
+
+static gboolean ButtonPressEventCB(GtkWidget *Widget,
+        GdkEventButton *Event, gpointer UserData) {
+    if (Surface == NULL) {
+        return FALSE;
+    }
+
+    if (Event->button == GDK_BUTTON_PRIMARY) {
+        DrawingBrush(Widget, Event->x, Event->y);
+    }
+
+    return TRUE;
+}
+
+
+
+
+
+
 
 void InitUI(GtkApplication *App, gpointer UserData) {
      
