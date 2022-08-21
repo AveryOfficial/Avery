f828e73c6d582a7963499222c6a15b234079a453
AveryOfficial <111689872+AveryOfficial@users.noreply.github.com>
2022-08-21 13:58:49 -0400

    Update mainwindow.c
    Another update.

--- src/gui/mainwindow.c
+++ src/gui/mainwindow.c
@@ -6,28 +6,43 @@
 void InitUI(GtkApplication *App, gpointer UserData) {
      
     GtkWidget *Window;
-    GtkWidget *Toolbox; //Toolbox is a paned window that will hold a series of buttons that will allow for selecting a different tool such as a vector brush, raster brush, or anything else that is needed at the time.
+    GtkWidget *Toolbox; //The current idea behind Toolbox is to hold a series of brushes and tools. Will implement later.
+    GtkWidget *Button; 
+    GtkWidget *VBox;
+    GtkWidget *MenuBar;
+    GtkWidget *FileMenu; //Not sure why this is required, consider removing later.
 
-    //These two labels are temporary. Will remove later.
-    GtkWidget *TL01;
-    GtkWidget *TL02;
-
+    GtkWidget *FMI; //File Menu Entry
+    GtkWidget *QMI; //Quit Menu Entry
+    
+    VBox = gtk_vbox_new(FALSE, 0);
     Window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_default_size(GTK_WINDOW(Window), 800, 600);
     gtk_window_set_title(GTK_WINDOW(Window), "Avery");
-
+    gtk_container_add(GTK_CONTAINER(Window), VBox);
     g_signal_connect(Window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    Button = gtk_button_new_with_label("TESTING!");
+    Toolbox = gtk_grid_new();
+    MenuBar = gtk_menu_bar_new();
+    FileMenu = gtk_menu_new();
 
-    Toolbox = gtk_paned_new(GTK_ORIENTATION_HORIZONTAL);
-    gtk_container_add(GTK_CONTAINER(Window), Toolbox);
-    TL01 = gtk_label_new("This space");
-    TL02 = gtk_label_new("for rent.");
+    FMI = gtk_menu_item_new_with_label("File");
+    QMI = gtk_menu_item_new_with_label("Quit");
 
-    gtk_paned_add1(GTK_PANED(Toolbox), TL01);
-    gtk_paned_add2(GTK_PANED(Toolbox), TL02);
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(FMI), FileMenu); //Rewrite this later. Seriously.
+    gtk_menu_shell_append(GTK_MENU_SHELL(FileMenu), QMI);
+    gtk_menu_shell_append(GTK_MENU_SHELL(MenuBar), FMI);
+    gtk_box_pack_start(GTK_BOX(VBox), MenuBar, FALSE, FALSE, 0);
+
+    gtk_container_add(GTK_CONTAINER(VBox), Toolbox);
+
+    gtk_alignment_set(Toolbox, 1, 0, 1, 1);
+
+    g_signal_connect(G_OBJECT(QMI), "activate",
+            G_CALLBACK(gtk_main_quit), NULL); //Clean this up later.
 
 
-
+    gtk_container_add(GTK_CONTAINER(Toolbox), Button); //This is temporary, will remove later.
     gtk_widget_show_all(Window);
 
 
